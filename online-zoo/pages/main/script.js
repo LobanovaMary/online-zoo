9fe36fa52d41d907a38681e1ee8117f4b4b84466
Lobanova Mary <lobanovama1@gmail.com>
2022-10-13 06:42:51 +0300

    feat: add testimonials pop-up

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -2,13 +2,12 @@ import BurgerMenu from "./module/BurgerMenu.js";
 import initAnimals from "./module/InitAnimals.js";
 import slider from "./module/Slider.js";
 import testimonials from "./module/Testimonials.js";
+import popUp from "./module/Pop-up.js";
+
 (function () {
   BurgerMenu();
   initAnimals();
   slider();
   testimonials();
+  popUp();
 })();
-
-alert(
-  "Здрайвствуйте, это моя первая работа. Проверте, пожалуйста, в четверг. Я буду еще доделовать"
-);
